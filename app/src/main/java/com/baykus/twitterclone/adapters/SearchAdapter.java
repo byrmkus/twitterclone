aa821aefab06be7a137f97419733ed4736a24105
byrmkus <bayram921992@gmail.com>
2021-05-24 22:06:24 +0300

    twitterclone 24.05.2021

--- app/src/main/java/com/baykus/twitterclone/adapters/SearchAdapter.java
+++ app/src/main/java/com/baykus/twitterclone/adapters/SearchAdapter.java
@@ -1,12 +1,22 @@
 package com.baykus.twitterclone.adapters;
 
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.baykus.twitterclone.activities.KisiTweetleriActivity;
 import com.baykus.twitterclone.databinding.KisiCardTasarimBinding;
 import com.baykus.twitterclone.pojo.Kullanicilar;
 import com.baykus.twitterclone.pojo.Tweetler;
@@ -28,8 +38,11 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.CardTasari
     @NonNull
     @Override
     public CardTasarimTutucu onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (kullanicilarList.size() == 0)
+            return null;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        KisiCardTasarimBinding kisiCardTasarimBinding = KisiCardTasarimBinding.inflate(inflater,parent,false);
+        KisiCardTasarimBinding kisiCardTasarimBinding = KisiCardTasarimBinding.inflate(inflater, parent, false);
+
 
         return new CardTasarimTutucu(kisiCardTasarimBinding);
     }
@@ -37,24 +50,58 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.CardTasari
     @Override
     public void onBindViewHolder(@NonNull CardTasarimTutucu holder, int position) {
 
-
         Kullanicilar kullanicilar = kullanicilarList.get(position);
         holder.binding.aramaAdsoyad.setText(kullanicilar.getAdsoyad());
         holder.binding.aramaMail.setText(kullanicilar.getMail());
-        holder.binding.aramaKullaniciadi.setText("@"+kullanicilar.getKullaniciadi());
+        holder.binding.aramaKullaniciadi.setText("@" + kullanicilar.getKullaniciadi());
 
 
-        if (!kullanicilar.getAvatar().equals("")){
+        if (!kullanicilar.getAvatar().equals("")) {
             Picasso.get().load(kullanicilar.getAvatar())
                     .memoryPolicy(MemoryPolicy.NO_CACHE)
                     .networkPolicy(NetworkPolicy.NO_CACHE)
                     .into(holder.binding.profileImageArama);
         }
 
+        holder.binding.cardViewArama.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("**************","********************");
+                Log.e("seçilen kullanıcı id ",kullanicilar.getId());
+                Intent intent =new Intent(v.getContext(),KisiTweetleriActivity.class);
+                intent.putExtra("id", kullanicilar.getId());
+                intent.putExtra("path", kullanicilar.getAvatar());
+                intent.putExtra("kullaniciadi", kullanicilar.getKullaniciadi());
+                intent.putExtra("adsoyad", kullanicilar.getAdsoyad());
+                intent.putExtra("mail", kullanicilar.getMail());
+
+                // Lolipop öncesi sürümlerde bu animasyon çalışmaz
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    View profil = holder.binding.profileImageArama;
+                    View adSoyad = holder.binding.aramaAdsoyad;
+                    View kullaniciAdi = holder.binding.aramaKullaniciadi;
+                    Pair<View, String> pairProfilFoto = Pair.create(profil, "profilImage");
+                    Pair<View, String> pairAdsoyad = Pair.create(adSoyad, "adsoyad");
+                    Pair<View, String> pairKullaniciadi = Pair.create(kullaniciAdi, "kullaniciadi");
+
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation((AppCompatActivity) mContext,
+                            pairProfilFoto,
+                            pairAdsoyad,
+                            pairKullaniciadi);
+                    mContext.startActivity(intent, options.toBundle());
+
+                } else {
+                    v.getContext().startActivity(intent);
+                }
+
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
+        if (kullanicilarList.size() == 0 || kullanicilarList == null)
+            return 0;
         return kullanicilarList.size();
     }
 
